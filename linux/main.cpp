83cd3fcf24035ae3018b0a048b91f31dcadb4342
JAePortfolio <jarena000@citymail.cuny.edu>
2020-03-02 18:53:30 -0500

    Changed how totalTime is calculated

--- linux/main.cpp
+++ linux/main.cpp
@@ -101,7 +101,8 @@ void speedometerFunction(){
   else if(wheelSensorGoLowCounter == 2){
     time(&currentTime_2);
 	printf("currentTime_2 %s", ctime(&currentTime_2));
-    timeDifferenceSeconds = difftime(currentTime_2.tv_usec,currentTime_1.tv_usec);
+    //timeDifferenceSeconds = difftime(currentTime_2,currentTime_1);
+	timeDifferenceSeconds = currentTime_2 - currentTime_1;
 	cout << "timeDifferenceSeconds:" << timeDifferenceSeconds << endl;
     speedometerReadingCalculation(timeDifferenceSeconds);
 	cout << "MPH:" << milesPerHour << endl;
